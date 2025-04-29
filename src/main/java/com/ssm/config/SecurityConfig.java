db72183c78cc5821b5b3bc22ad33d75c39a0aa7d
NOITATIDME <chgt186@naver.com>
2025-04-29 17:08:04 +0900

    security config 초안 설정

--- src/main/java/com/ssm/config/SecurityConfig.java
+++ src/main/java/com/ssm/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,7 +21,17 @@ public class SecurityConfig {
     @Bean
     protected SecurityFilterChain swaggerSecurityFilterChain(HttpSecurity http) throws Exception {
     	http
-    	.csrf(AbstractHttpConfigurer::disable)
+        // rest api 설정
+        .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화 -> cookie를 사용하지 않으면 꺼도 된다. (cookie를 사용할 경우 httpOnly(XSS 방어), sameSite(CSRF 방어)로 방어해야 한다.)
+        .cors(AbstractHttpConfigurer::disable) // cors 비활성화 -> 프론트와 연결 시 따로 설정 필요
+        .httpBasic(AbstractHttpConfigurer::disable) // 기본 인증 로그인 비활성화
+        .formLogin(AbstractHttpConfigurer::disable) // 기본 login form 비활성화
+        .logout(AbstractHttpConfigurer::disable) // 기본 logout 비활성화
+        .headers(c -> c.frameOptions(
+                FrameOptionsConfig::disable).disable()) // X-Frame-Options 비활성화
+        .sessionManagement(c ->
+                c.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용하지 않음
+        
     	.authorizeHttpRequests(auth -> auth.requestMatchers(
                 "/swagger-ui/**",
                 "/v3/api-docs/**",
@@ -31,8 +43,6 @@ public class SecurityConfig {
             ).permitAll()
     			.anyRequest().authenticated() // 그 외 경로는 허용
             )
-    	 .formLogin(Customizer.withDefaults()) // 기본 로그인 페이지 사용
-         .httpBasic(Customizer.withDefaults()) // Swagger가 쓰는 Basic Auth 대응
     	;
 
         return http.build(); // ⬅️ SecurityFilterChain Bean 반환
